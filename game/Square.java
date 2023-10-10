0335f503cc648a2c1d11f1323d2e7f9527e3e077
PolyZone <sorenn.dk@gmail.com>
2023-10-10 16:32:37 +0200

    square code

---
+++ game/Square.java
@@ -0,0 +1,31 @@
+public class Square {
+	String nameToken;
+	int moneyValue;
+	boolean extraTurn;
+
+	public String getNameToken() {
+		return this.nameToken;
+	}
+
+	public void setNameToken(String newName) {
+		this.nameToken = newName;
+	}
+
+	public int getMoneyValue() {
+		return this.moneyValue;
+	}
+
+	public void setMoneyValue(int newValue) {
+		this.moneyValue = newValue;
+	}
+
+	public boolean getExtraTurn() {
+		return this.extraTurn;
+	}
+
+	public void setExtraTurn(boolean newTurn) {
+		this.extraTurn = newTurn;
+
+	}
+
+}
