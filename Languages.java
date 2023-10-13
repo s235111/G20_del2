b0634c2876de4926776070bb12b7fbeaf4e6b779
Alex Lundberg <lundberg.alex1@gmail.com>
2023-10-13 17:44:06 +0200

    Added method to print current language files

--- Languages.java
+++ Languages.java
@@ -1,9 +1,30 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FilenameFilter;
 import java.util.Scanner;
 import java.util.HashMap;
 
 class Languages {
+
+	public static void main(String[] args){
+		printLanguages();
+	}
+
+	public static void printLanguages(){
+		File folder = new File(".");
+		FilenameFilter filter = new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+                return name.endsWith(".csv");  // Filters for .csv files
+            }
+        };
+
+		File[] listOfFiles = folder.listFiles(filter);
+
+		for(File e: listOfFiles){
+			System.out.println(e);
+		}
+}
+
 
     public static HashMap parse(String fileName){
         HashMap<String,String> map = new HashMap<>();
