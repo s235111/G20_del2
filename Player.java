854d63f2ad2db6991ce2fa4ec08d5eede2e775c6
Alex Lundberg <lundberg.alex1@gmail.com>
2023-10-14 21:09:58 +0200

    Added base Player class

---
+++ Player.java
@@ -0,0 +1,30 @@
+class Player {
+
+	private String name;
+	private Account account;
+
+	// Constructor
+	public Player() {
+		this.name = "";
+		this.account = new Account();
+	}
+
+	// Constructor with name overload
+	public Player(String givenName) {
+		this.name = givenName;
+		this.account = new Account();
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String nameToSet) {
+		this.name = nameToSet;
+	}
+
+	public static Account getAccount(){
+		return this.account;
+	}
+
+}
