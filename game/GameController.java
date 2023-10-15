49e4e2c89575b38d198d26b6cb7754169763b6f6
Andreas Jensen <s235455@dtu.dk>
2023-10-15 11:48:14 +0200

    Added all squares

--- game/GameController.java
+++ game/GameController.java
@@ -1,4 +1,15 @@
 public class GameController {
+	Square Tower = new Square("Tower", 250, false);
+	Square Crater = new Square("Crater", -100, false);
+	Square PalaceGates = new Square("Palace gates", 100, false);
+	Square ColdDesert = new Square("Cold Desert", -20, false);
+	Square WalledCity = new Square("Walled City", 180, false);
+	Square Monastery = new Square("Monastery", 0, false);
+	Square BlackCave = new Square("Black cave", -70, false);
+	Square HutsInTheMountain = new Square("Huts in the mountain", 60, false);
+	Square TheWerewall = new Square("The Werewall", -80, true);
+	Square ThePit = new Square("The pit", -50, false);
+	Square Goldmine = new Square("Goldmine", 650, false);
 
 	public static void main(String[] args) {
 
