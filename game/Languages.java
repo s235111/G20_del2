dbd0687472fe882361b92c239332e8fd4ae10a3b
Worsøe <s235111@dtu.dk>
2023-10-23 09:29:26 +0200

    Fix formatting in Languages.java

--- game/Languages.java
+++ game/Languages.java
@@ -6,44 +6,44 @@ import java.util.HashMap;
 
 class Languages {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println(chooseLanguage());
 	}
 
-	public static String[] getAvailableLanguages(){
+	public static String[] getAvailableLanguages() {
 		File folder = new File("./languages");
 		FilenameFilter filter = new FilenameFilter() {
-            public boolean accept(File dir, String name) {
-                return name.endsWith(".csv");  // Filters for .csv files
-            }
-        };
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".csv"); // Filters for .csv files
+			}
+		};
 		File[] listOfFiles = folder.listFiles(filter);
 		String[] fileNames = new String[listOfFiles.length];
-		for(int i = 0; i < listOfFiles.length; i++){
+		for (int i = 0; i < listOfFiles.length; i++) {
 			fileNames[i] = listOfFiles[i].getName();
 		}
 		return fileNames;
 	}
 
-	public static HashMap getLanguage(String language){
+	public static HashMap getLanguage(String language) {
 		return parse(language + ".csv");
 	}
 
-	public static HashMap chooseLanguage(){
+	public static HashMap chooseLanguage() {
 
 		boolean languageChosen = false;
 		Scanner sc = new Scanner(System.in);
 		File langFile;
 		String playerChoice = "English.csv";
-		while(languageChosen != true){
+		while (languageChosen != true) {
 			System.out.println("Choose language:");
 			printLanguages();
 			playerChoice = sc.nextLine() + ".csv";
 			langFile = new File(playerChoice);
 
-			if (langFile.exists()){
+			if (langFile.exists()) {
 				languageChosen = true;
-			} else{
+			} else {
 				System.out.println("The chosen file does not exist, please note your selection is case sensitive, try again");
 			}
 		}
@@ -52,45 +52,43 @@ class Languages {
 		return parse(playerChoice);
 	}
 
-	public static void printLanguages(){
+	public static void printLanguages() {
 		File folder = new File(".");
 		FilenameFilter filter = new FilenameFilter() {
-            public boolean accept(File dir, String name) {
-                return name.endsWith(".csv");  // Filters for .csv files
-            }
-        };
+			public boolean accept(File dir, String name) {
+				return name.endsWith(".csv"); // Filters for .csv files
+			}
+		};
 
 		File[] listOfFiles = folder.listFiles(filter);
 
 		System.out.println("Below you see all the languages currently supported");
 
-		for(File e: listOfFiles){
+		for (File e : listOfFiles) {
 			System.out.println(e.getName().split(".csv")[0]);
 		}
-}
+	}
 
-
-    public static HashMap parse(String fileName){
-        HashMap<String,String> map = new HashMap<>();
-        String data;
-        String[] dataParsed;
+	public static HashMap parse(String fileName) {
+		HashMap<String, String> map = new HashMap<>();
+		String data;
+		String[] dataParsed;
 
 		// try required to not get error
-        try{
-        File lang = new File(fileName);
-        Scanner sc = new Scanner(lang);
-        while(sc.hasNextLine()){
-            data = sc.nextLine();
-            dataParsed = data.split(",", 2);
-            map.put(dataParsed[0], dataParsed[1]);
-        }
-        sc.close();
-        } catch (FileNotFoundException e){
-            System.out.println("error");
-        }
+		try {
+			File lang = new File(fileName);
+			Scanner sc = new Scanner(lang);
+			while (sc.hasNextLine()) {
+				data = sc.nextLine();
+				dataParsed = data.split(",", 2);
+				map.put(dataParsed[0], dataParsed[1]);
+			}
+			sc.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("error");
+		}
 
-        return map;
-    }
-
+		return map;
+	}
 
 }
