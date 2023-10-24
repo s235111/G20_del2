9558b621e6adecf671cca013b725a5a0cb3262b8
Worsøe <s235111@dtu.dk>
2023-10-24 15:17:39 +0200

    Remove extraneous methods in Languages

--- game/Languages.java
+++ game/Languages.java
@@ -8,10 +8,6 @@ import java.util.HashMap;
 
 public class Languages {
 
-	public static void main(String[] args) {
-		System.out.println(chooseLanguage());
-	}
-
 	public static String[] getAvailableLanguages() {
 		File folder = new File("resources");
 		FilenameFilter filter = new FilenameFilter() {
@@ -33,46 +29,6 @@ public class Languages {
 			return parse("resources/" + language + ".csv");
 		} catch (FileNotFoundException e) {
 			return null;
-		}
-	}
-
-	public static HashMap<String, String> chooseLanguage() {
-
-		boolean languageChosen = false;
-		Scanner sc = new Scanner(System.in);
-		File langFile;
-		String playerChoice = "English.csv";
-		while (languageChosen != true) {
-			System.out.println("Choose language:");
-			printLanguages();
-			playerChoice = sc.nextLine() + ".csv";
-			langFile = new File(playerChoice);
-
-			if (langFile.exists()) {
-				languageChosen = true;
-			} else {
-				System.out.println("The chosen file does not exist, please note your selection is case sensitive, try again");
-			}
-		}
-		sc.close();
-
-		return parse(playerChoice);
-	}
-
-	public static void printLanguages() {
-		File folder = new File(".");
-		FilenameFilter filter = new FilenameFilter() {
-			public boolean accept(File dir, String name) {
-				return name.endsWith(".csv"); // Filters for .csv files
-			}
-		};
-
-		File[] listOfFiles = folder.listFiles(filter);
-
-		System.out.println("Below you see all the languages currently supported");
-
-		for (File e : listOfFiles) {
-			System.out.println(e.getName().split(".csv")[0]);
 		}
 	}
 
